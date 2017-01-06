2f38cc676bfde9a24cace58a22431aaee18bf366
John Amakye <john.amakye@minneapolismn.gov>
2017-01-06 13:16:23 -0600

    assignment completed

--- queries.js
+++ queries.js
@@ -11,5 +11,8 @@
     db.bios.find({awards: {$elemMatch: {award: "Turing Award"}}}).pretty();
 
 // Find documents with IDs between 3 and 7.
+    db.bios.find({_id: {$gt: 3, $lt: 7}}).pretty();
 // Find documents with awards that were awarded before the year 2000.
+    db.bios.find({awards: {$elemMatch:{year: {$lt:2000}}}}).pretty();
 // Find documents with birth dates, but no death dates.
+    db.bios.find({birth: {$exists: true} , death: {$exists:false}}).pretty();
